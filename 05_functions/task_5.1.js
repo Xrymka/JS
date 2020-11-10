f46612ce3c3c0b46442f3a70fa378504f9274682
Kate Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2020-11-10 23:14:52 +0300

    Added HW5

--- 05_functions/task_5.1.js
+++ 05_functions/task_5.1.js
@@ -1,3 +1,22 @@
 // Задание
 // Напишите функцию, которая создаёт массив email-адресов, не попавших в чёрный список. В качестве аргументов функция должна принимать: массив строк с исходными email адресами, массив строк с email адресами в чёрном списке.
 // В конце файла с кодом домашнего задания напишите конструкцию export default {название функции}, чтобы была возможность автоматической проверки получившейся функции.
+
+
+function checkedList(firstList, secondList) {
+	let elementsNotInSecondList = [];
+	for (let i in firstList) {
+		if (!secondList.includes(firstList[i])) {
+			elementsNotInSecondList.push(firstList[i]);
+		}
+	};
+	return elementsNotInSecondList;
+};
+
+
+emailsList= ['gergrg@ff.ru', 'geergrg@ff.ru', 'kjhkjhh@ff.ru', 'huhiuh@ff.ru', 'ihiu@ff.ru', 'yuhu7@ff.ru', 'kjoij@ff.ru', 'srgsergreg@ger.com'];
+emailsBlackList = ['kjoij@ff.ru', 'gergrg@ff.ru', '877n@ff.ru', 'oiio8@ff.ru', '546464@ff.ru', '6564klj@ff.ru', '51oj@ff.ru', 'iouh78@ff.ru', 'kny7@ff.ru'];
+
+console.log(checkedList(emailsList, emailsBlackList));
+
+// export default {checkedList};
