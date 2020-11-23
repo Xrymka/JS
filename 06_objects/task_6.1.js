4f64a50f930b0c2e9c8637e33eddc51688346eaa
Kate Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2020-11-24 01:31:39 +0300

    UPD: task 6.1

--- 06_objects/task_6.1.js
+++ 06_objects/task_6.1.js
@@ -1,28 +1,31 @@
 // Задание 1
 // Напишите функцию, фильтрующую массив объектов по значению свойства. Массив, название свойства и нужное значение должны передаваться в качестве аргументов.
 
-
-
-
 let objects = [
-{ name: 'Василий', surname: 'Васильев' },
-{ name: 'Иван', surname: 'Иванов' },
-{ name: 'Пётр', surname: 'Петров' }
+	{ name: 'Василий', surname: 'Васильев' },
+	{ name: 'Иван', surname: 'Кузов' },
+	{ name: 'Иван', surname: 'Иванов' },
+	{ name: 'Пётр', surname: 'Петров' }
 ]
 
 function arrayFilterByProperty(list, property, value) {
-	for (let i in list) {
-	  let newArray = [];
-	  if (list[property] == list[i] && list[value] == list[i]) {
-		result.push(newArray);
-	  }
-	}
-	return result;
-  }
+	let result = [];
 
+	for (val of list) {
+		for (let key in val) {
+			if (key === property && val[key] === value) {
+				//console.log(val.name + " " + val.surname);
+				result.push(val);
+			}
+		}
+	}
+
+	return result;
+}
 
 let result = arrayFilterByProperty(objects, 'name', 'Иван');
+for (val of result) {
+    console.log(val);
+}
 
-
-  console.log(result, objects);
 // export default {checkedList};
